a9b5d52ffc9dc9d2fe06edc336e68604479b5c48
heike <hofmann@iastate.edu>
2020-02-27 10:13:56 -0600

    update code

--- R/ud_plot.R
+++ R/ud_plot.R
@@ -8,7 +8,7 @@ if(getRversion() >= "2.15.1")  utils::globalVariables(c("base", "c1F",
 # Main function--------------
 
 ud_plot <- function(outPrep, b=0, totperc="yes", vscale=NULL, labelvar=NULL,
-                    groupColour="none", ud_control=ud_colours()) {
+                    levelColour="none", barColour=levelColour, ud_control=ud_colours()) {
 
   INX <- outPrep$data
   levs <- outPrep$levs
@@ -38,61 +38,65 @@ ud_plot <- function(outPrep, b=0, totperc="yes", vscale=NULL, labelvar=NULL,
   # Set the requested colours
   cy <- ud_control$colours
   gcpal <- ud_control$gcpal
+  
+  # Check colour parameters
+  if(!(levelColour %in% c(levs, "none"))) warning("levelColour name is not a level variable and will have no effect", call.=FALSE)
+  
+  glev <- case_when(
+      levelColour==levs[1] ~ 1,
+      levelColour==levs[2] ~ 2,
+      levelColour==levs[3] ~ 3,
+      TRUE ~ 0
+    )
 
-  # Check groupColour
-  if(!(groupColour %in% c(names(INX), "none"))) {
-    stop("groupColour name not in the dataset", call.=FALSE)
-  } else
-    if (!(groupColour=="none")) {
-      INX$groupColour <- INX[[groupColour]]
-      lgC <- length(unique(INX$groupColour))
+  # Check barColour variable and that there are enough colours in the palette for barColour
+  if(!(barColour %in% c(names(INX), "none"))) stop("barColour name not in the dataset", call.=FALSE)
+  if (!(barColour=="none")) {
+      INX$barColour <- INX[[barColour]]
+    if (!(class(INX$barColour) %in% c("character", "factor"))) {
+      stop("barColour should be of class character or factor", call. = FALSE)
+      }
+      lgC <- length(unique(INX$barColour))
            if (lgC > length(gcpal)) {
         set.seed(4711)
         gcpal2 <- distinctColorPalette(lgC)
       } else {
       gcpal2 <- gcpal
       }
-
+    }
+  
+ # Check, if barColour is not a grouping variable, whether it can be used
+    if(glev > 0) {
+    txy <- table(INX[, c(levs[glev], "barColour")])
+    txyd <- data.frame(txy)
+ # You want only 1 entry in each row of the table
+    if(sum(txyd$Freq>0) > dim(txy)[1]) {
+    stop(" the barColour variable cannot be used for colouring the levelColour level", call. = FALSE)
+    }
     }
 
-  # Check for nesting of top level in groupColour (which is not in levs)
-  colNest <- 0
-  if(!(groupColour %in% c(levs, "none"))) {
-    txy <- with(INX, table(c1F, groupColour))
-    txyd <- data.frame(txy)
-    colNest <- ifelse(sum(txyd$Freq>0) > max(dim(txy)), 0, ifelse(dim(txy)[1] > dim(txy)[2], 1, 0))
-  }
-
-  hxS <- sum(hx$NestL)
-  levCol <- 0
-  if (hxS > 0 & !(groupColour=="none")) levCol <- 1
-  if (hxS == 0) {
-    levCol <- case_when(
-      groupColour==levs[1] ~ 1,
-      groupColour==levs[2] ~ 2,
-      groupColour==levs[3] ~ 3,
-      TRUE ~ 0
-    )
-  }
-  
-
+    
   # Calculate the cumulatives for the three levels (c1F, c2F, c3F)
-  if (colNest==0) INX1 <- INX %>% mutate(totp=100*(sum(wt*v2)/sum(wt*v1)-1)) %>% group_by(c1F) %>%
+  if (!(glev==1)) INX1 <- INX %>% mutate(totp=100*(sum(wt*v2)/sum(wt*v1)-1)) %>% group_by(c1F) %>%
     summarise(base=sum(wt*v1), perc=100*(sum(wt*v2)/base-1), totp=mean(totp)) %>% ungroup() %>%
     mutate(cumX=cumsum(base))
-  if (colNest==1) INX1 <- INX %>% mutate(totp=100*(sum(wt*v2)/sum(wt*v1)-1)) %>% group_by(c1F) %>%
-    summarise(base=sum(wt*v1), perc=100*(sum(wt*v2)/base-1), totp=mean(totp), groupColour=unique(groupColour)) %>% ungroup() %>%
+  if (glev==1) INX1 <- INX %>% mutate(totp=100*(sum(wt*v2)/sum(wt*v1)-1)) %>% group_by(c1F) %>%
+    summarise(base=sum(wt*v1), perc=100*(sum(wt*v2)/base-1), totp=mean(totp), barColour=unique(barColour)) %>% ungroup() %>%
     mutate(cumX=cumsum(base))
-  low1 <- min(INX1$perc)
-  high1 <- max(INX1$perc)
+    low1 <- min(INX1$perc)
+    high1 <- max(INX1$perc)
   if (lc > 1) {
-    INX2 <- INX %>% group_by(c1F, c2F) %>% summarise(mc2base=sum(wt*v1), mc2perc=100*(sum(wt*v2)/mc2base-1)) %>%
+    if (!(glev==2)) INX2 <- INX %>% group_by(c1F, c2F) %>% summarise(mc2base=sum(wt*v1), mc2perc=100*(sum(wt*v2)/mc2base-1)) %>%
+      ungroup() %>% mutate(c2s=cumsum(mc2base))
+     if (glev==2) INX2 <- INX %>% group_by(c1F, c2F) %>% summarise(mc2base=sum(wt*v1), mc2perc=100*(sum(wt*v2)/mc2base-1), barColour=unique(barColour)) %>%
       ungroup() %>% mutate(c2s=cumsum(mc2base))
     low2 <- min(INX2$mc2perc)
     high2 <- max(INX2$mc2perc)
   }
   if (lc==3) {
-    INX3 <- INX %>% group_by(c1F, c2F, c3F) %>% summarise(mc3base=sum(wt*v1), mc3perc=100*(sum(wt*v2)/mc3base-1)) %>%
+    if (!(glev==3)) INX3 <- INX %>% group_by(c1F, c2F, c3F) %>% summarise(mc3base=sum(wt*v1), mc3perc=100*(sum(wt*v2)/mc3base-1)) %>%
+      ungroup() %>% mutate(c3s=cumsum(mc3base))
+        if (glev==3) INX3 <- INX %>% group_by(c1F, c2F, c3F) %>% summarise(mc3base=sum(wt*v1), mc3perc=100*(sum(wt*v2)/mc3base-1), barColour=unique(barColour)) %>%
       ungroup() %>% mutate(c3s=cumsum(mc3base))
     low3 <- min(INX3$mc3perc)
     high3 <- max(INX3$mc3perc)
@@ -101,32 +105,26 @@ ud_plot <- function(outPrep, b=0, totperc="yes", vscale=NULL, labelvar=NULL,
   # Plot
   # If you want a legend   + theme(legend.title=element_blank(), legend.position="bottom")
   h1 <- ggplot(INX1, aes(xmin=lag(cumX, default=0), xmax=cumX, ymin=b, ymax=perc))
-  if (colNest==1) h1 <- h1 + geom_rect(aes(fill=groupColour), col=cy[2]) + theme(legend.title=element_blank(), legend.position="bottom") + scale_fill_manual(values=gcpal2)
-  if (colNest==0 & levCol==1) {
-  lgC3 <- length(unique(INX$c1F))
-           if (lgC3 > length(gcpal)) {
-        set.seed(4711)
-        gcpal3 <- distinctColorPalette(lgC3)
-      } else {
-      gcpal3 <- gcpal
+  if (glev==1) {
+  h1 <- h1 + geom_rect(aes(fill=barColour), col=cy[2]) + theme(legend.title=element_blank(), legend.position="bottom") + scale_fill_manual(values=gcpal2)
       }
-  h1 <- h1 + geom_rect(aes(fill=c1F), col=cy[2]) + theme(legend.title=element_blank(), legend.position="bottom") + scale_fill_manual(values=gcpal3)
+  if (!(glev==1)) {
+  h1 <- h1 + geom_rect(fill=cy[1], col=cy[2])
   }
-  if (colNest==0 & !(levCol==1)) h1 <- h1 + geom_rect(fill=cy[1], col=cy[2])
   if (length(vscale)==2) h1 <- h1  + coord_cartesian(ylim=c(vscale[1], vscale[2]))
   if (totperc=="yes") h4 <- h1 + geom_hline(yintercept=INX1$totp, linetype="dashed", col=cy[3])
   if (totperc=="no") h4 <- h1
   if (lc==2) {
-    if (levCol==2) h2 <- h1 + geom_rect(data=INX2, aes(xmin=lag(c2s, default=0), xmax=c2s, ymin=b, ymax=mc2perc, fill=c2F), col=cy[4]) + scale_fill_manual(values=gcpal2) + theme(legend.title=element_blank(), legend.position="bottom")
-    if (!(levCol==2)) h2 <- h1 + geom_rect(data=INX2, aes(xmin=lag(c2s, default=0), xmax=c2s, ymin=b, ymax=mc2perc), alpha=0, col=cy[4])
+    if (glev==2) h2 <- h1 + geom_rect(data=INX2, aes(xmin=lag(c2s, default=0), xmax=c2s, ymin=b, ymax=mc2perc, fill=barColour), col=cy[4]) + scale_fill_manual(values=gcpal2) + theme(legend.title=element_blank(), legend.position="bottom")
+    if (!(glev==2)) h2 <- h1 + geom_rect(data=INX2, aes(xmin=lag(c2s, default=0), xmax=c2s, ymin=b, ymax=mc2perc), alpha=0, col=cy[4])
     if (totperc=="yes") h4 <- h2 + geom_hline(yintercept=INX1$totp, linetype="dashed", col=cy[3])
     if (totperc=="no") h4 <- h2
   }
   if (lc==3) {
-    if (levCol==3) h2 <- h1 + geom_rect(data=INX3, aes(xmin=lag(c3s, default=0), xmax=c3s, ymin=b, ymax=mc3perc, fill=c3F), col=cy[5]) + scale_fill_manual(values=gcpal2) + theme(legend.title=element_blank(), legend.position="bottom")
-    if (!(levCol==3)) h2 <- h1 + geom_rect(data=INX3, aes(xmin=lag(c3s, default=0), xmax=c3s, ymin=b, ymax=mc3perc), alpha=0, col=cy[5])
-    if (levCol==2) h3 <- h2 + geom_rect(data=INX2, aes(xmin=lag(c2s, default=0), xmax=c2s, ymin=b, ymax=mc2perc, fill=c2F), col=cy[4]) + scale_fill_manual(values=gcpal2) + theme(legend.title=element_blank(), legend.position="bottom")
-    if (!(levCol==2)) h3 <- h2 + geom_rect(data=INX2, aes(xmin=lag(c2s, default=0), xmax=c2s, ymin=b, ymax=mc2perc), alpha=0, col=cy[4])
+    if (glev==3) h2 <- h1 + geom_rect(data=INX3, aes(xmin=lag(c3s, default=0), xmax=c3s, ymin=b, ymax=mc3perc, fill=barColour), col=cy[5]) + scale_fill_manual(values=gcpal2) + theme(legend.title=element_blank(), legend.position="bottom")
+    if (!(glev==3)) h2 <- h1 + geom_rect(data=INX3, aes(xmin=lag(c3s, default=0), xmax=c3s, ymin=b, ymax=mc3perc), alpha=0, col=cy[5])
+    if (glev==2) h3 <- h2 + geom_rect(data=INX2, aes(xmin=lag(c2s, default=0), xmax=c2s, ymin=b, ymax=mc2perc, fill=barColour), col=cy[4]) + scale_fill_manual(values=gcpal2) + theme(legend.title=element_blank(), legend.position="bottom")
+    if (!(glev==2)) h3 <- h2 + geom_rect(data=INX2, aes(xmin=lag(c2s, default=0), xmax=c2s, ymin=b, ymax=mc2perc), alpha=0, col=cy[4])
     if (totperc=="yes") h4 <- h3 + geom_hline(yintercept=INX1$totp, linetype="dashed", col=cy[3])
     if (totperc=="no") h4 <- h3
   }
